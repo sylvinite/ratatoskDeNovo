22f351bdc3a070167f746447fa0b263fcbf01b5c
root <root@localhost.localdomain>
2013-09-13 16:09:10 +0200

    Fixed haloplex again

--- ratatosk/ratatosk/pipeline/__init__.py
+++ ratatosk/ratatosk/pipeline/__init__.py
@@ -1,6 +1,6 @@
 import os
 import ratatosk
-from ratatosk.pipeline import align, seqcap, pippen
+from ratatosk.pipeline import align, haloplex, seqcap, pippen
 
 # Define configuration file  locations and classes for predefined workflows
 config_dict = {
@@ -8,6 +8,13 @@ config_dict = {
                   'cls':None},
     'Align' : {'config' : os.path.join(ratatosk.__path__[0], os.pardir, "config", "align.yaml"),
                'cls' : align.Align},
+    'HaloPlex' : {'config' : os.path.join(ratatosk.__path__[0], os.pardir, "config", "haloplex.yaml"),
+               'cls' : haloplex.HaloPlex},
+    'HaloPlexSummary' : {'config' : os.path.join(ratatosk.__path__[0], os.pardir, "config", "haloplex.yaml"),
+               'cls' : haloplex.HaloPlexSummary},
+    'HaloPlexCombine' : {'config' : os.path.join(ratatosk.__path__[0], os.pardir, "config", "haloplex.yaml"),
+               'cls' : haloplex.HaloPlexCombine},
+
     'Pippen': {'config' : os.path.join(ratatosk.__path__[0], os.pardir, "config", "pippen.yaml"),
 		'cls' : pippen.Pippen},
     'Seqcap' : {'config' : os.path.join(ratatosk.__path__[0], os.pardir, "config", "seqcap.yaml"),
